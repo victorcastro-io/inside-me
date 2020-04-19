ef6ba99138de62fb622a5503a62f903ff8ff5fbc
victor.castro.contreras <castro002>
2020-04-19 08:20:54 -0500

    [INI] Fist commit

---
+++ inside-me/controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  inside-me
+//
+//  Created by Victor Hugo on 4/18/20.
+//  Copyright © 2020 Victor Hugo. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ inside-me/controllers/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  inside-me
+//
+//  Created by Victor Hugo on 4/17/20.
+//  Copyright © 2020 Victor Hugo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var comeInto: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func comeIntoAction(_ sender: Any) {
+        
+    }
+    
+}
+
