01fea0e944a5ca015af3a309f524523b6a3b5f40
Victor Castro <63174838+victorcastro-io@users.noreply.github.com>
2020-04-19 21:40:58 -0500

    Biometric (#1)
    * [IMP] Login screen
    
    * [IMP] TouchID and swith typeID
    
    * [IMP] Diferenciacion de imagen (TouchID/FaceID)
    
    Co-authored-by: victor.castro.contreras <castro002>

--- inside-me/controllers/LoginViewController.swift
+++ inside-me/controllers/LoginViewController.swift
@@ -7,19 +7,87 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var comeInto: UIButton!
+    @IBOutlet weak var biometricIdButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        authBiometricID()
     }
 
     @IBAction func comeIntoAction(_ sender: Any) {
         
     }
-    
+        
+    @IBAction func AuthBiometricClick(_ sender: Any) {
+        authBiometricID()
+    }
+
+    func authBiometricID() {
+        let currentType = LAContext().biometricType
+        
+         switch currentType {
+             case .touchID:
+                 biometricIdButton.setImage(UIImage(named: "biometric_touchid"), for: .normal)
+             
+             case .faceID:
+                 biometricIdButton.setImage(UIImage(named: "biometric_faceid"), for: .normal)
+             default:
+                 biometricIdButton.isHidden = true
+             }
+        
+        let authContext = LAContext()
+        let authReason = Bundle.main.infoDictionary?["NSFaceIDUsageDescription"] as! String
+        print(authReason)
+        var authError: NSError?
+        
+        if authContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: authReason, reply: { (success, error) -> Void in
+                if success {
+                    DispatchQueue.main.async {
+                        print("Authenticated!")
+                        self.performSegue(withIdentifier: "segueToHome", sender: nil)
+                    }
+                } else {
+                    print("Error auth")
+                }
+            })
+        }
+    }
 }
 
+
+extension LAContext {
+    enum BiometricType: String {
+        case none
+        case touchID
+        case faceID
+    }
+
+    var biometricType: BiometricType {
+        var error: NSError?
+
+        guard self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            // Capture these recoverable error thru Crashlytics
+            return .none
+        }
+
+        if #available(iOS 11.0, *) {
+            switch self.biometryType {
+                case .touchID:
+                    return .touchID
+                case .faceID:
+                    return .faceID
+                default:
+                    return .none
+            }
+        } else {
+            return self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) ? .touchID : .none
+        }
+    }
+}
